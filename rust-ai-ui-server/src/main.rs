526b7d8b1406f97dbd03c7a3ff0505956387c1ab
Panakotta00 <panakotta00@gmail.com>
2023-03-26 16:24:07 +0200

    Websocket stuff or so

--- rust-ai-ui-server/src/main.rs
+++ rust-ai-ui-server/src/main.rs
@@ -1,13 +1,35 @@
+#![feature(async_closure)]
+
 #[macro_use] extern crate rocket;
 
 pub mod model;
 
-use async_graphql::{EmptyMutation, EmptySubscription, Schema, http::GraphiQLSource};
+use std::io::Sink;
+use std::ops::Deref;
+use std::pin::Pin;
+use std::str::FromStr;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize};
+use async_graphql::{EmptyMutation, Schema, http::GraphiQLSource, Request, Response, Data, futures_util};
+use async_graphql::futures_util::future::Ready;
+use async_graphql::http::WebSocketProtocols;
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
-use rocket::{response::content, routes, State};
+use futures_core::stream::BoxStream;
+use rocket::{
+    get, routes, State,
+    response::content,
+    fairing::{AdHoc, Fairing},
+    http::{Header, hyper::header::{ACCESS_CONTROL_ALLOW_CREDENTIALS, ACCESS_CONTROL_ALLOW_ORIGIN}},
+    response::websocket::{WebsocketMessage, WebsocketChannel, CreateWebsocket, Websocket},
+};
+use rocket::http::ext::IntoCollection;
+use rocket::http::hyper::body::Bytes;
 use crate::model::{QueryRoot, SubscriptionRoot};
+use tokio_stream::{Stream};
+use futures::{Future, pin_mut, StreamExt, TryFutureExt};
 
-pub type GraphQLSchema = Schema<QueryRoot, EmptyMutation, SubscriptionRoot>;
+pub type GraphQLSchemaValue = Schema<QueryRoot, EmptyMutation, SubscriptionRoot>;
+pub type GraphQLSchema = Arc<GraphQLSchemaValue>;
 
 #[get("/")]
 fn index() -> &'static str {
@@ -16,12 +38,13 @@ fn index() -> &'static str {
 
 #[get("/playground")]
 fn graphiql() -> content::RawHtml<String> {
-    content::RawHtml(GraphiQLSource::build().endpoint("/graphql").subscription_endpoint("/graphql").finish())
+    content::RawHtml(GraphiQLSource::build().endpoint("/graphql").subscription_endpoint("/graphql/subscription").finish())
 }
 
 #[get("/graphql?<query..>")]
 async fn graphql_query(schema: &State<GraphQLSchema>, query: GraphQLQuery) -> GraphQLResponse {
-    query.execute(schema.inner()).await
+    let meep = schema.inner().deref();
+    query.execute(schema.inner().deref()).await
 }
 
 #[post("/graphql", data = "<request>", format = "application/json")]
@@ -29,13 +52,170 @@ async fn graphql_request(
     schema: &State<GraphQLSchema>,
     request: GraphQLRequest,
 ) -> GraphQLResponse {
-    request.execute(schema.inner()).await
+    request.execute(&*schema.inner().deref()).await
 }
+
+struct MyConfig {
+    count: AtomicUsize,
+}
+
+/*#[get("/graphql/subscription")]
+async fn graphql_subscription(state: &State<MyConfig>, mut shutdown: Shutdown) -> EventStream![] {
+    let counter = AtomicUsize::new(0);
+    let stream = EventStream! {
+        let mut interval = time::interval(Duration::from_secs(1));
+        loop {
+            let count = counter.fetch_add(1, Ordering::Relaxed);
+            select! {
+                _ = interval.tick() => {
+                    println!("ping! {}", count);
+                    yield Event::data(format!("ping! {}", count));
+                },
+                _ = &mut shutdown => {
+                    println!("shutdown!");
+                    break;
+                }
+            };
+        }
+    };
+    stream.heartbeat(Duration::from_secs(20))
+}*/
+
+use async_stream::stream;
+use rocket::form::validate::len;
+use tokio::io::{AsyncReadExt, AsyncWrite, AsyncWriteExt};
+use tokio::sync::Mutex;
+
+/*struct TestSocket {
+    schema: GraphQLSchema,
+    protocol: async_graphql::http::WebSocketProtocols,
+}
+
+#[rocket::async_trait]
+impl rocket::Upgrade<'static> for TestSocket {
+    async fn start(&self, mut upgraded: rocket::http::hyper::upgrade::Upgraded) {
+        // let ws = self.websocket.unwrap();
+
+        upgraded.write_all(b"meep").await;
+        println!("Sent!!!");
+
+        let mut vec = Vec::new();
+        upgraded.read_to_end(&mut vec).await;
+        println!("recieved: {:?}", String::from_utf8(vec));
+
+        /*let mut buf = vec![0; 128];
+        loop {
+            let data = upgraded.(&mut buf).await;
+            //let meep = buf.clone();
+            if let Ok(len) = data {
+                if len > 0 {
+                    println!("text: {:?}", &buf[..len]);
+                }
+            }
+        }*/
+
+        let stream = stream!{
+            println!("meep");
+            yield &[0; 10]
+        };
+
+        /*let mut ws = async_graphql::http::WebSocket::new(
+            self.schema.deref().clone(),
+            stream,
+            self.protocol
+        ).on_connection_init(|_: serde_json::Value| -> Ready<async_graphql::Result<_>> {
+            futures_util::future::ready(Ok(Data::default()))
+        }).map(|msg| match msg {
+            async_graphql::http::WsMessage::Text(text) => {
+                println!("text: {}", text.as_str());
+            },
+            async_graphql::http::WsMessage::Close(code, status) => {
+                println!("code: {:?} status: {:?}", code, status.as_str());
+            },
+        }).map(Ok).forward(futures::sink()).await;*/
+    }
+}
+
+impl<'r> rocket::response::Responder<'r, 'r> for TestSocket {
+    fn respond_to(mut self, req: &rocket::Request<'_>) -> rocket::response::Result<'r> {
+        self.protocol = async_graphql::http::WebSocketProtocols::from_str(req.headers().get_one("sec-websocket-protocol").unwrap_or("")).unwrap_or(WebSocketProtocols::SubscriptionsTransportWS);
+        println!("protocol: {:?}", self.protocol);
+        rocket::Response::build()
+            .status(rocket::http::Status::SwitchingProtocols)
+            .raw_header("Connection", "upgrade")
+            .raw_header("Upgrade", "websocket")
+            .upgrade(Some(Box::new(self)))
+            .ok()
+    }
+}
+
+#[get("/graphql/subscription")]
+async fn graphql_subscription(schema: &State<GraphQLSchema>, state: &State<MyConfig>) -> TestSocket {
+    TestSocket{
+        schema: schema.inner().clone(),
+        protocol: WebSocketProtocols::SubscriptionsTransportWS,
+    }
+}*/
+
+
+#[get("/graphql/subscription")]
+async fn graphql_subscription(schema: &State<GraphQLSchema>, state: &State<MyConfig>) -> Websocket<impl Fn(WebsocketChannel) -> Box<dyn futures::Future<Output = ()> + Send + Unpin>> {
+    let websocket = CreateWebsocket! {
+        let mut ch: WebsocketChannel = ch;
+
+        //let input_stream = stream!{
+            tokio::select!{
+                _ = async {
+                    while let Some(msg) = ch.next().await {
+                        println!("Message: {msg:?}")
+                    }
+                } => {},
+            }
+        //};
+    };
+
+    //let graphql = async_graphql::http::WebSocket::new(schema, input_stream, Protocols::);
+    websocket
+}
+
+/*#[get("/graphql/subscription")]
+async fn graphql_subscription(schema: &State<GraphQLSchema>, state: &State<MyConfig>) -> Websocket<impl Fn(WebsocketChannel) -> Box<dyn futures::Future<Output = ()> + Send + Unpin>> {
+    let websocket = accept_async(request).await.unwrap();
+
+    let (incoming, outgoing) = websocket.split();
+    let mut incoming = incoming.fuse();
+    //let mut outgoing = outgoing.fuse();
+
+    tokio::select! {
+        result = async {
+            loop {
+                let message = incoming.next().await.unwrap().unwrap();
+                let message = match message {
+                    Message::Text(text) => text,
+                    _ => continue,
+                };
+                let message = format!("{}: {}", message);
+                outgoing.send();
+                tx.send(Message::Text(message.clone())).unwrap();
+            }
+        } => {
+        }
+        result = async {
+            while let Some(message) = rx.recv().await {
+                outgoing.send(message).await.unwrap();
+            }
+        } => {}
+    }
+}*/
 
 #[launch]
 fn rocket() -> _ {
-    GraphQlSub
-    let schema = Schema::build(QueryRoot, EmptyMutation, SubscriptionRoot).finish();
+    let schema = Arc::new(Schema::build(QueryRoot, EmptyMutation, SubscriptionRoot).finish());
 
-    rocket::build().manage(schema).mount("/", routes![index, graphql_query, graphql_request, graphiql])
+    rocket::build().manage(schema).manage(MyConfig{count: AtomicUsize::new(0)}).attach(AdHoc::on_response("CORS", |_, res| {
+        Box::pin(async move {
+            res.set_header(Header::new(ACCESS_CONTROL_ALLOW_ORIGIN.as_str(), "*"));
+            res.remove_header(ACCESS_CONTROL_ALLOW_CREDENTIALS.as_str());
+        })
+    })).mount("/", routes![index, graphql_query, graphql_request, graphql_subscription, graphiql])
 }
--- rust-ai-ui-website/src/main.rs
+++ rust-ai-ui-website/src/main.rs
@@ -1,5 +1,11 @@
-use web_sys::HtmlInputElement;
+use web_sys::{HtmlInputElement};
+use wasm_bindgen::{JsCast, UnwrapThrowExt};
+use web_sys::console::log_1;
+use ws_stream_wasm::{WsMeta, WsEvent, WsMessage};
 use yew::prelude::*;
+use futures::{Stream, StreamExt};
+use wasm_bindgen_futures::JsFuture;
+use wasm_bindgen::prelude::*;
 
 #[derive(Properties, PartialEq)]
 struct MessageListProps {
@@ -18,41 +24,132 @@ fn message_list(props: &MessageListProps) -> Html {
     }).collect()
 }
 
-#[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
+pub enum AppMsg {
+    SSE(String),
+    NewMsg(String),
+    Increment,
+}
+
+pub struct App {
+    messages: Vec<String>,
+    input: NodeRef,
+    count: u64,
+}
+
+impl Component for App {
+    type Message = AppMsg;
+    type Properties = ();
+
+    fn create(ctx: &Context<Self>) -> Self {
+        /*let client = eventsource_client::ClientBuilder::for_url("https://example.com/stream")?
+            .body("fuck you all!!!!".to_string())
+            .build();
+
+        let link = ctx.link().clone();
+        let stream = client
+            .stream()
+            .map_ok(|event| {
+                log_1(&"Message!".to_string().into());
+                match event {
+                    es::SSE::Event(e) => {
+                        println!("got an event: {}\n{}", e.event_type, e.data)
+                        link.send_message(AppMsg::SSE(e.data.as_string().unwrap()));
+                    }
+                    es::SSE::Comment(comment) => {
+                        println!("got a comment: \n{}", comment)
+                    }
+                }
+            })
+            .map_err(|err| eprintln!("error streaming events: {:?}", err));
+
+        tokio::spawn(async move {
+            while let Ok(Some(_)) = stream.try_next().await {}
+        });*/
+
+        wasm_bindgen_futures::spawn_local(async move {
+            let (mut ws_meta, mut wsio) = WsMeta::connect("ws://127.0.0.1:8090/graphql/subscription", None).await.unwrap();
+
+            //let mut evts = ws_meta.observe(Default::default()).expect_throw( "observe" );
+
+            let pin = std::pin::Pin::new(&mut wsio);
+
+            while let Some(msg) = wsio.next().await {
+                match msg {
+                    WsMessage::Text(text) => {
+                        log_1(&text.into());
+                    }
+                    WsMessage::Binary(_) => {}
+                }
+            }
+
+            /*while let Ok(Some(event)) = evts.try_next().await {
+                let event: WsEvent = event;
+                match  event {
+                    WsEvent::Open => {
+                        log_1(&"Connected!".to_string().into());
+                    }
+                    WsEvent::Error => {}
+                    WsEvent::Closing => {}
+                    WsEvent::Closed(_) => {
+                        log_1(&"Closed!".to_string().into());
+                    }
+                    WsEvent::WsErr(_) => {}
+                }
+            }*/
+        });
+
+        Self{
+            messages: Default::default(),
+            input: NodeRef::default(),
+            count: 0,
         }
-    };
+    }
 
-    let messages = use_state(|| vec!["Fuck you!", "No, fuck you!", "Shut the fuck up!"].iter().map(|v| v.to_string()).collect::<Vec<_>>());
-    let input = NodeRef::default();
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            AppMsg::SSE(msg) => {
+                self.messages.push(msg);
+                true
+            },
+            AppMsg::NewMsg(msg) => {
+                self.messages.push(msg);
+                true
+            },
+            AppMsg::Increment => {
+                self.count += 1;
+                true
+            }
+        }
+    }
 
-    let submit_new_message = {
-        let messages = messages.clone();
-        let input = input.clone();
-        Callback::from(move |event: SubmitEvent| {
-            println!("meep");
-            event.prevent_default();
-            let mut msgs = messages.to_vec();
-            msgs.push(input.cast::<HtmlInputElement>().unwrap().value());
-            messages.set(msgs);
-        })
-    };
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let input = NodeRef::default();
 
-    html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
-            <MessageList messages={(*messages).clone()} />
-            <form onsubmit={submit_new_message}>
-                <input ref={&input} />
-            </form>
-        </div>
+        let submit_new_message = {
+            let input = self.input.clone();
+            ctx.link().callback(move |event: SubmitEvent| {
+                event.prevent_default();
+                let msg = input.cast::<HtmlInputElement>().unwrap().value();
+                AppMsg::NewMsg(msg.clone())
+            })
+        };
+
+        let increment_click = {
+            ctx.link().callback(move |_| AppMsg::Increment)
+        };
+
+        log_1(&self.messages.len().to_string().into());
+
+        html! {
+            <div>
+                <button onclick={increment_click}>{ "+1" }</button>
+                <p>{self.count}</p>
+                <MessageList messages={self.messages.clone()} />
+                <form onsubmit={submit_new_message}>
+                    <input ref={&self.input} />
+                </form>
+            </div>
+        }
     }
 }
 
